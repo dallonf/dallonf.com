952b24b78bf6a42fa1df86675005b5dcbee7b9a8
Dallon Feldner <dallonfdev@gmail.com>
2022-08-15 16:14:57 -0500

    Set up open props and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: [
+        "open-props",
+        "@fontsource/clear-sans",
+        "@fontsource/dm-sans",
+      ],
+    },
+  },
+});
