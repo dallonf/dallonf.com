f9b18aaec55732945114dbe388e9de75f1f2433d
Dallon Feldner <dallonfdev@gmail.com>
2025-02-01 17:32:00 -0600

    Update remaining packages

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,9 @@ const path = require("path");
 
 module.exports = {
   plugins: [
-    require("postcss-custom-media")({
-      importFrom: [
-        path.join(__dirname, "node_modules/open-props/media.min.css"),
-      ],
+    require("@csstools/postcss-global-data")({
+      files: [path.join(__dirname, "node_modules/open-props/media.min.css")],
     }),
+    require("postcss-custom-media")(),
   ],
 };
