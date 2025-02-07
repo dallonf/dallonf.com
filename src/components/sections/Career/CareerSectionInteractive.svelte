f54733960b5b7de888b98fd11389bdb92f4b8d4c
Dallon Feldner <dallonfdev@gmail.com>
2025-02-07 09:22:35 -0600

    Move some logic into the outer component so Timeline can focus just on adapting the lib to Svelte

--- src/components/sections/Career/CareerSectionInteractive.svelte
+++ src/components/sections/Career/CareerSectionInteractive.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
+  import { Temporal } from "temporal-polyfill";
   import Timeline from "./Timeline.svelte";
+
+  const today = Temporal.Now.plainDateISO();
 </script>
 
-<Timeline />
+<Timeline
+  events={[
+    { content: "Sibi", start: "2022-09-01", end: today.toString() },
+    {
+      content: "Volusion",
+      start: "2020-11-01",
+      end: "2022-06-01",
+    },
+    {
+      content: "RigUp",
+      start: "2019-06-01",
+      end: "2020-01-01",
+    },
+    {
+      content: "Phunware",
+      start: "2016-06-01",
+      end: "2019-04-01",
+    },
+    {
+      content: "Nuve",
+      start: "2015-08-01",
+      end: "2016-05-01",
+    },
+  ]}
+/>
--- src/components/sections/Career/Timeline.svelte
+++ src/components/sections/Career/Timeline.svelte
@@ -1,43 +1,22 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { Temporal } from "temporal-polyfill";
-  import { Timeline } from "vis-timeline";
+  import { Timeline, type DataItemCollectionType } from "vis-timeline";
 
-  let container = $state<HTMLDivElement | null>();
-  let timeline = $state<Timeline | null>();
+  interface Props {
+    events: DataItemCollectionType;
+  }
+
+  const { events }: Props = $props();
+
+  let container = $state<HTMLDivElement>();
+  let timeline = $state<Timeline>();
 
   onMount(() => {
-    const today = Temporal.Now.plainDateISO();
+    timeline = new Timeline(container!, events, {});
+  });
 
-    console.log({ today, container });
-
-    timeline = new Timeline(
-      container!,
-      [
-        { content: "Sibi", start: "2022-09-01", end: today.toString() },
-        {
-          content: "Volusion",
-          start: "2020-11-01",
-          end: "2022-06-01",
-        },
-        {
-          content: "RigUp",
-          start: "2019-06-01",
-          end: "2020-01-01",
-        },
-        {
-          content: "Phunware",
-          start: "2016-06-01",
-          end: "2019-04-01",
-        },
-        {
-          content: "Nuve",
-          start: "2015-08-01",
-          end: "2016-05-01",
-        },
-      ],
-      {}
-    );
+  $effect(() => {
+    timeline?.setItems(events);
   });
 </script>
 
