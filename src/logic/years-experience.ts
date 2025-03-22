77c302f02349fd3b5d82348628cd20edf49a7501
Dallon Feldner <dallonfdev@gmail.com>
2025-03-22 15:45:11 -0500

    Documentation about how I calculate my years of experience

--- src/logic/years-experience.ts
+++ src/logic/years-experience.ts
@@ -1,8 +1,23 @@
 import { Temporal } from "temporal-polyfill";
 
+/**
+ * Estimated time I started working with React.
+ * About halfway through my time at GameSalad.
+ */
 const REACT_START = new Temporal.PlainDate(2015, 1, 1);
+/**
+ * When I started my engineering career (at Terralever), I was working part-time.
+ * I don't feel quite right about counting those as a full years of experience,
+ * but it's not nothing either, so I count them as half a year.
+ * 
+ * I also don't remember exactly when I started at Terralever, so I'm rounding up
+ * to the end of the month.
+ */
+const PARTTIME_START = new Temporal.PlainDate(2010, 8, 30);
+/**
+ * I started working full-time at InMotion Software.
+ */
 const FULLTIME_START = new Temporal.PlainDate(2013, 8, 1);
-const PARTTIME_START = new Temporal.PlainDate(2010, 8, 30); // don't remember the exact date; it was later in the month iirc
 
 export function reactYears(): number {
   const now = Temporal.Now.plainDateISO();
